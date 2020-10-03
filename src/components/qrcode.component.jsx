9f88bcb7c5a87467a4580f2b5a2ac534f2d67529
Israel Torres <israel.a.torres01@gmail.com>
2020-10-03 16:04:40 -0400

    first commit

---
+++ src/components/qrcode.component.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+var QRCode = require("qrcode.react");
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.itorres-solutions.com/">
+        ITorres Solutions
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(16, 0, 6),
+    height: "82vh",
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  qrcode: {
+    marginTop: "30px",
+  },
+  footer: {
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(6),
+  },
+}));
+
+export default function Qrcode() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            QR Code
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Scan QR Code
+            </Typography>
+            <QRCode
+              className={classes.qrcode}
+              value="https://www.itorres-solutions.com/"
+            />
+          </Container>
+        </div>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
